a832ee5fdfadd12b728e2bdc187f0cef2556ff8d
Taras Glek <taras@glek.net>
2023-06-11 23:36:30 +0300

    cleanup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 pip install -qU ctranslate2 transformers[torch] accelerate einops
-time ct2-transformers-converter --model mosaicml_mpt-7b-storywriter/ --output_dir mosaicml_mpt-7b-storywriter.ct2 --trust_remote_code --low_cpu_mem_usage --quantization float16
+git clone https://huggingface.co/mosaicml/mpt-7b-instruct
+time ct2-transformers-converter --model mpt-7b-instruct/ --output_dir "mpt-7b-instruct.ct2.int8" --trust_remote_code --low_cpu_mem_usage --quantization int8
+time python run.py < input.txt
--- run.py
+++ run.py
@@ -4,10 +4,11 @@ import sys
 import os
 import time
 # export CT2_VERBOSE=1
-os.environ["CT2_VERBOSE"] = "1"
+# os.environ["CT2_VERBOSE"] = "1"
 
-generator = ctranslate2.Generator("mosaicml_mpt-7b-storywriter.ct2", device="cpu", inter_threads=4, intra_threads=4)
+# generator = ctranslate2.Generator("mosaicml_mpt-7b-storywriter.ct2", device="cpu", inter_threads=4, intra_threads=4)
 # generator = ctranslate2.Generator("mpt-7b-storywriter-ct2.fp8", device="cuda")
+generator = ctranslate2.Generator("mpt-7b-instruct.ct2.int8", device="cuda")
 
 tokenizer = transformers.AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
 
@@ -15,7 +16,8 @@ print("input text:")
 prompt = sys.stdin.read()
 prompt_tokens = tokenizer.convert_ids_to_tokens(tokenizer.encode(prompt))
 #  https://github.com/OpenNMT/CTranslate2/issues/1127#issuecomment-1514401355
-if False:
+print(prompt, end ="", flush=True)
+if True:
     start = time.time()
     num_tokens = 0
 
@@ -23,14 +25,14 @@ if False:
             prompt_tokens,
             # sampling_temperature=0.8,
             sampling_topk=10,
-            max_length=2048,
+            max_length=700,
         )
     for result in results:
-        print(result.token, end =" ", flush=True)
+        print(tokenizer.decode(result.token_id), end ="", flush=True)
         num_tokens += 1
 
     end = time.time()
-    print("Tokens per second:", num_tokens / (end - start))
+    print(f"\nTokens per second:{num_tokens / (end - start)} {end-start}s", file=sys.stderr)
 else:
     results = generator.generate_batch([prompt_tokens], max_length=700, sampling_topk=10)
 
