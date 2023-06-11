14b0a3e27e1ae1c22d1afd9d45e02da05a823a0f
Taras Glek <taras@glek.net>
2023-06-11 16:14:19 +0300

    does something

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 import ctranslate2
 import transformers
+import sys
+generator = ctranslate2.Generator("mosaicml_mpt-7b-storywriter.ct2", device="cpu", inter_threads=4, intra_threads=4)
 
-generator = ctranslate2.Generator("mpt-7b-storywriter-ct2")
 tokenizer = transformers.AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
 
-prompt = "Long long time ago, "
+print("input text:")
+prompt = sys.stdin.read()
 tokens = tokenizer.convert_ids_to_tokens(tokenizer.encode(prompt))
 # 
 results = generator.generate_batch([tokens], max_length=256, sampling_topk=10)
 # 
 text = tokenizer.decode(results[0].sequences_ids[0])
+print(text)
