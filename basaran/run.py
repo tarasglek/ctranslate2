c8fad6b41ffca3dc10ea4c829a91f18059d5656f
Taras Glek <taras@glek.net>
2023-06-23 13:58:58 +0300

    checkpoint

--- basaran/run.py
+++ basaran/run.py
@@ -4,14 +4,15 @@ import time
 torch.set_num_threads(1)
 from basaran.model import load_model
 # model = 'gpt2'
-model = 'google/flan-t5-xl'
-# model = 'mosaicml/mpt-7b-instruct'
+# model = 'MBZUAI/LaMini-Flan-T5-783M'
+# model = 'google/flan-t5-xl'
+model = 'mosaicml/mpt-7b-instruct'
 # model = 'mosaicml/mpt-7b-storywriter'
-load_in_8bit = True
-streaming_model = load_model(model,  trust_remote_code=True, load_in_8bit=load_in_8bit,)
+load_in_8bit = False
+streaming_model = load_model(model,  trust_remote_code=True, load_in_4bit=load_in_8bit,)
 
 print(f"Memory footprint of {model}: {streaming_model.model.get_memory_footprint()}")
-
+print(f"Model input limit: {streaming_model.tokenizer.max_model_input_sizes}")
 start = time.time()
 num_tokens = 0
 for choice in streaming_model(sys.stdin.read(), max_tokens=1000, echo=True):
--- hf.py
+++ hf.py
@@ -4,26 +4,39 @@ limiting threads:
 import torch
 use_cpu = False
 # if use_cpu:
-num_threads = 6
+num_threads = 4
 torch.set_num_threads(num_threads)
 # else:
-device = torch.device("cuda")
+devicedevice = torch.device("cuda")
+# devicedevice = torch.device("cpu")
+
+from transformers import BitsAndBytesConfig
+
 import transformers
 import sys
 
 
 
 from transformers import AutoTokenizer
-tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
+device_map = {
+        "transformer.word_embeddings": 0,
+            "transformer.word_embeddings_layernorm": 0,
+                "lm_head": "cpu",
+                    "transformer.h": 0,
+                        "transformer.ln_f": 0,
+                        }
 
+quantization_config = BitsAndBytesConfig(llm_int8_enable_fp32_cpu_offload=True)
+tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
 prompt = sys.stdin.read()
 # prompt_tokens = tokenizer.convert_ids_to_tokens(tokenizer.encode(prompt))
 prompt_tokens = tokenizer([prompt], return_tensors="pt")
 prompt_token_count = prompt_tokens.input_ids.shape[1]
 print(f"Input token count: {prompt_token_count}")
 
-name = 'mosaicml/mpt-7b-storywriter'
-
+name = 'mosaicml/mpt-7b-instruct'
+# name = 'ehartford/WizardLM-30B-Uncensored'
+# name = 'emozilla/mpt-7b-storywriter-fast'
 config = transformers.AutoConfig.from_pretrained(name, trust_remote_code=True, load_in_8bit=True)
 config.max_seq_len = 83968 # (input + output) tokens can now be up to 83968
 
@@ -33,6 +46,7 @@ model = transformers.AutoModelForCausalLM.from_pretrained(
   trust_remote_code=True,
   load_in_8bit=True,
   device_map="auto",
+  quantization_config=quantization_config
 )
 print(f"Memory footprint of {name}: {model.get_memory_footprint()}")
 
